be16aad272d61bb496f7202ea9f749ca0f47125c
Marcin Polewski <marcin.polewski@gmail.com>
2019-03-17 22:40:10 -0500

    updates

--- MMS.IdentityManagement/AuthenticationRequest.cs
+++ MMS.IdentityManagement/AuthenticationRequest.cs
@@ -1,9 +1,20 @@
-﻿namespace MMS.IdentityManagement
+﻿using System;
+
+namespace MMS.IdentityManagement
 {
     public class AuthenticationRequest
     {
         public string ReaderId { get; set; }
         public int? MemberId { get; set; }
         public string KeyCode { get; set; }
+    }
+
+    public class AuthenticationResult
+    {
+        public string AccessToken { get; set; }
+        public string AccessTokenType { get; set; }
+        public DateTimeOffset ExpiresWhen { get; set; }
+        public Member Member { get; set; }
+        public string[] Roles { get; set; }
     }
 }
--- MMS.IdentityManagement/Member.cs
+++ MMS.IdentityManagement/Member.cs
@@ -5,7 +5,8 @@ namespace MMS.IdentityManagement
     public class Member
     {
         public int MemberId { get; set; }
-        public string FullName { get; set; }
+        public string FamilyName { get; set; }
+        public string GivenName { get; set; }
         public DateTimeOffset JoinedWhen { get; set; }
         public DateTimeOffset ExpiresWhen { get; set; }
     }
