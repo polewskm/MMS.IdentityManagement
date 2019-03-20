4ada16241fcf71b97bdf841701f8f2b26e394246
Marcin Polewski <marcin.polewski@gmail.com>
2019-03-20 06:48:26 -0500

    models

--- MMS.IdentityManagement/AuthenticationRequest.cs
+++ MMS.IdentityManagement/AuthenticationRequest.cs
@@ -1,20 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace MMS.IdentityManagement
 {
     public class AuthenticationRequest
     {
-        public string ReaderId { get; set; }
+        public string ClientId { get; set; }
         public int? MemberId { get; set; }
         public string KeyCode { get; set; }
+        public string[] Scope { get; set; }
     }
 
     public class AuthenticationResult
     {
         public string AccessToken { get; set; }
         public string AccessTokenType { get; set; }
+        public DateTimeOffset IssuedWhen { get; set; }
         public DateTimeOffset ExpiresWhen { get; set; }
         public Member Member { get; set; }
-        public string[] Roles { get; set; }
+        public ClaimsIdentity Identity { get; set; }
     }
 }
