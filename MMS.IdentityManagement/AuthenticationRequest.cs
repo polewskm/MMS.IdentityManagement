cbe2b0be6f1184e538a0fa752258755948d44ea2
Marcin Polewski <marcin.polewski@gmail.com>
2019-03-21 15:35:56 -0500

    updates

--- MMS.IdentityManagement/AuthenticationRequest.cs
+++ MMS.IdentityManagement/AuthenticationRequest.cs
@@ -1,20 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Security.Claims;
 
 namespace MMS.IdentityManagement
 {
-    public class AuthenticationRequest
+    // POST /api/v1/tokens/keycode
+    // POST /api/v1/tokens/refresh
+
+    public abstract class AuthenticationRequest
     {
         public string ClientId { get; set; }
-        public int? MemberId { get; set; }
+    }
+
+    public class KeyCodeAuthenticationRequest : AuthenticationRequest
+    {
         public string KeyCode { get; set; }
-        public string[] Scope { get; set; }
     }
 
     public class AuthenticationResult
     {
         public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+
         public string AccessTokenType { get; set; }
         public DateTimeOffset IssuedWhen { get; set; }
         public DateTimeOffset ExpiresWhen { get; set; }
--- MMS.IdentityManagement/Member.cs
+++ MMS.IdentityManagement/Member.cs
@@ -5,8 +5,7 @@ namespace MMS.IdentityManagement
     public class Member
     {
         public int MemberId { get; set; }
-        public string FamilyName { get; set; }
-        public string GivenName { get; set; }
+        public string DisplayName { get; set; }
         public DateTimeOffset JoinedWhen { get; set; }
         public DateTimeOffset ExpiresWhen { get; set; }
     }
