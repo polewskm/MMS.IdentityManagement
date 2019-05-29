b0a7ed9754d66f1c0c8301c40de475a371de0ce4
Marcin Polewski <marcin.polewski@gmail.com>
2019-05-29 16:20:02 -0500

    updates

--- MMS.IdentityManagement.Api.Test/Services/ClientValidatorTests.cs
+++ MMS.IdentityManagement.Api.Test/Services/ClientValidatorTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using MMS.IdentityManagement.Api.Data;
 using MMS.IdentityManagement.Api.Models;
@@ -11,63 +12,13 @@ namespace MMS.IdentityManagement.Api.Test.Services
 {
     public class ClientValidatorTests
     {
-        public static IEnumerable<object[]> TestData => new[]
-        {
-            new object[]
-            {
-                new ClientValidationRequest
-                {
-                    // nothing
-                },
-                "invalid_request",
-                "Missing client_id",
-            },
-            new object[]
-            {
-                new ClientValidationRequest
-                {
-                    ClientId = string.Empty,
-                },
-                "invalid_request",
-                "Missing client_id",
-            },
-        };
-
-        [Theory]
-        [MemberData(nameof(TestData))]
-        public async Task ValidateClientAsync_GivenError_ThenFailedResult(ClientValidationRequest request, string expectedError, string expectedErrorDescription)
-        {
-            var mockRepository = new Mock<IClientRepository>(MockBehavior.Strict);
-            var mockSecretProtectorSelector = new Mock<ISecretProtectorSelector>(MockBehavior.Strict);
-
-            var clientValidator = new ClientValidator(mockRepository.Object, mockSecretProtectorSelector.Object);
-            var result = await clientValidator.ValidateClientAsync(request).ConfigureAwait(false);
-
-            Assert.False(result.Success);
-            Assert.Equal(expectedError, result.Error);
-            Assert.Equal(expectedErrorDescription, result.ErrorDescription);
-        }
-
         [Fact]
         public async Task ValidateClientAsync_GivenMissingClientId_ThenInvalidRequest()
         {
-            var mockRepository = new Mock<IClientRepository>(MockBehavior.Strict);
+            var mockClientRepository = new Mock<IClientRepository>(MockBehavior.Strict);
             var mockSecretProtectorSelector = new Mock<ISecretProtectorSelector>(MockBehavior.Strict);
-            //var mockSecretProtector = new Mock<ISecretProtector>(MockBehavior.Strict);
 
-            //const string cipherType = "";
-
-            //mockSecretProtectorSelector
-            //    .Setup(_ => _.Select(cipherType))
-            //    .Returns(mockSecretProtector.Object)
-            //    .Verifiable();
-
-            //mockSecretProtector
-            //    .Setup(_ => _.CipherType)
-            //    .Returns(cipherType)
-            //    .Verifiable();
-
-            var clientValidator = new ClientValidator(mockRepository.Object, mockSecretProtectorSelector.Object);
+            var clientValidator = new ClientValidator(mockClientRepository.Object, mockSecretProtectorSelector.Object);
 
             var request = new ClientValidationRequest();
             var result = await clientValidator.ValidateClientAsync(request).ConfigureAwait(false);
@@ -75,6 +26,165 @@ namespace MMS.IdentityManagement.Api.Test.Services
             Assert.False(result.Success);
             Assert.Equal("invalid_request", result.Error);
             Assert.Equal("Missing client_id", result.ErrorDescription);
+        }
+
+        [Fact]
+        public async Task ValidateClientAsync_GivenEmptyClientId_ThenInvalidRequest()
+        {
+            var mockClientRepository = new Mock<IClientRepository>(MockBehavior.Strict);
+            var mockSecretProtectorSelector = new Mock<ISecretProtectorSelector>(MockBehavior.Strict);
+
+            var clientValidator = new ClientValidator(mockClientRepository.Object, mockSecretProtectorSelector.Object);
+
+            var request = new ClientValidationRequest
+            {
+                ClientId = string.Empty
+            };
+            var result = await clientValidator.ValidateClientAsync(request).ConfigureAwait(false);
+
+            Assert.False(result.Success);
+            Assert.Equal("invalid_request", result.Error);
+            Assert.Equal("Missing client_id", result.ErrorDescription);
+        }
+
+        [Fact]
+        public async Task ValidateClientAsync_GivenUnknownClientId_ThenInvalidClient()
+        {
+            var mockClientRepository = new Mock<IClientRepository>(MockBehavior.Strict);
+            var mockSecretProtectorSelector = new Mock<ISecretProtectorSelector>(MockBehavior.Strict);
+
+            var clientValidator = new ClientValidator(mockClientRepository.Object, mockSecretProtectorSelector.Object);
+
+            var clientId = Guid.NewGuid().ToString("N");
+            var cancellationToken = CancellationToken.None;
+
+            mockClientRepository
+                .Setup(_ => _.GetClientByIdAsync(clientId, cancellationToken))
+                .ReturnsAsync((Client)null)
+                .Verifiable();
+
+            var request = new ClientValidationRequest
+            {
+                ClientId = clientId,
+            };
+            var result = await clientValidator.ValidateClientAsync(request, cancellationToken).ConfigureAwait(false);
+
+            Assert.False(result.Success);
+            Assert.Equal("invalid_client", result.Error);
+            Assert.Equal("Unknown client_id", result.ErrorDescription);
+            Assert.Null(result.Client);
+            Assert.Null(result.Secret);
+
+            mockClientRepository.Verify();
+        }
+
+        [Fact]
+        public async Task ValidateClientAsync_GivenValidClientId_ThenValidClient()
+        {
+            var mockClientRepository = new Mock<IClientRepository>(MockBehavior.Strict);
+            var mockSecretProtectorSelector = new Mock<ISecretProtectorSelector>(MockBehavior.Strict);
+
+            var clientValidator = new ClientValidator(mockClientRepository.Object, mockSecretProtectorSelector.Object);
+
+            var clientId = Guid.NewGuid().ToString("N");
+            var cancellationToken = CancellationToken.None;
+            var client = new Client
+            {
+                Id = clientId,
+                Disabled = false,
+                RequireSecret = false,
+            };
+
+            mockClientRepository
+                .Setup(_ => _.GetClientByIdAsync(clientId, cancellationToken))
+                .ReturnsAsync(client)
+                .Verifiable();
+
+            var request = new ClientValidationRequest
+            {
+                ClientId = clientId,
+            };
+            var result = await clientValidator.ValidateClientAsync(request, cancellationToken).ConfigureAwait(false);
+
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
+            Assert.Null(result.ErrorDescription);
+            Assert.Same(client, result.Client);
+
+            mockClientRepository.Verify();
+        }
+
+        [Fact]
+        public async Task ValidateClientAsync_GivenDisabledClientId_ThenUnauthorizedClient()
+        {
+            var mockClientRepository = new Mock<IClientRepository>(MockBehavior.Strict);
+            var mockSecretProtectorSelector = new Mock<ISecretProtectorSelector>(MockBehavior.Strict);
+
+            var clientValidator = new ClientValidator(mockClientRepository.Object, mockSecretProtectorSelector.Object);
+
+            var clientId = Guid.NewGuid().ToString("N");
+            var cancellationToken = CancellationToken.None;
+            var client = new Client
+            {
+                Id = clientId,
+                Disabled = true,
+                RequireSecret = false,
+            };
+
+            mockClientRepository
+                .Setup(_ => _.GetClientByIdAsync(clientId, cancellationToken))
+                .ReturnsAsync(client)
+                .Verifiable();
+
+            var request = new ClientValidationRequest
+            {
+                ClientId = clientId,
+            };
+            var result = await clientValidator.ValidateClientAsync(request, cancellationToken).ConfigureAwait(false);
+
+            Assert.False(result.Success);
+            Assert.Equal("unauthorized_client", result.Error);
+            Assert.Equal("Disabled client", result.ErrorDescription);
+            Assert.Null(result.Client);
+
+            mockClientRepository.Verify();
+        }
+
+        [Fact]
+        public async Task ValidateClientAsync_GivenNullSecret_ThenInvalidRequest()
+        {
+            var mockClientRepository = new Mock<IClientRepository>(MockBehavior.Strict);
+            var mockSecretProtectorSelector = new Mock<ISecretProtectorSelector>(MockBehavior.Strict);
+
+            var clientValidator = new ClientValidator(mockClientRepository.Object, mockSecretProtectorSelector.Object);
+
+            var clientId = Guid.NewGuid().ToString("N");
+            var cancellationToken = CancellationToken.None;
+            var client = new Client
+            {
+                Id = clientId,
+                Disabled = false,
+                RequireSecret = true,
+            };
+
+            mockClientRepository
+                .Setup(_ => _.GetClientByIdAsync(clientId, cancellationToken))
+                .ReturnsAsync(client)
+                .Verifiable();
+
+            var request = new ClientValidationRequest
+            {
+                ClientId = clientId,
+                ClientSecret = null,
+            };
+            var result = await clientValidator.ValidateClientAsync(request, cancellationToken).ConfigureAwait(false);
+
+            Assert.False(result.Success);
+            Assert.Equal("unauthorized_client", result.Error);
+            Assert.Equal("Disabled client", result.ErrorDescription);
+            Assert.Null(result.Client);
+
+            mockClientRepository.Verify();
         }
 
     }
